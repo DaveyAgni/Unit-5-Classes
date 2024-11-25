c6e732469fe381dbd6de6c5844feed790b099a17
DaveyAgni <devarchit.agnihotri@icloud.com>
2024-11-25 10:18:31 -0500

    Update Cup.java

--- Cup.java
+++ Cup.java
@@ -1,14 +1,66 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
+import java.util.Arrays;
 
-//design your Cup class
-//write all code for your Cup class here
-//test your Cup class with the CupRunner
+public class Cup
+{
+    private double limCup;
+    private double amtCup = 0;
+    private int howManyAdd = 0;
 
-//make a Cup
+    public Cup(int limCup)
+    {
+        this.limCup = limCup;
+    }
 
-	//add needed instance variables
-	//you will need more than 1
-	
-	//add constructors and methods
+    public double getAmtInCup()
+    {
+        return amtCup;
+    }
+
+    public double add(int a)
+    {
+        amtCup = a + amtCup;
+        howManyAdd++;
+        return amtCup;
+    }
+
+    public boolean isFull()
+    {
+        if(amtCup >= limCup)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean willOverflow(int n)
+    {
+        if(amtCup + n >= limCup)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public int getNumAdds()
+    {
+        return howManyAdd;
+    }
+
+    public double pour(int lost)
+    {
+        amtCup = amtCup - lost;
+        return amtCup;
+    }
+
+    public double addLots(double[] i)
+    {
+        amtCup =  (Arrays.stream(i).sum()) + amtCup;
+        return amtCup;
+    }
+
+}
